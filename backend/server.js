fa892a88a12a9740c7485a7ae324246406007b34
2303A52161 <2303A52161@sru.edu.in>
2026-06-14 00:33:33 +0530

    changed vercel link

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://medi-hist-blond.vercel.app'],
+  origin: ['http://localhost:5173', 'https://medi-hist-blond.vercel.app/login'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
